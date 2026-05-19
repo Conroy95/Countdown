01d09415b9723b7add1104f77cff367b5bab055f
Conroy95 <leandervdneut@ziggo.nl>
2026-05-19 09:07:58 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Centrale functie om werkdagen te berekenen tot een specifieke datum
 function getWorkingDaysLeft(targetDate, freeDaysArray = []) {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
@@ -21,22 +20,20 @@ function getWorkingDaysLeft(targetDate, freeDaysArray = []) {
     return count;
 }
 
-// Hier voer je de berekeningen uit zodra de pagina laadt
 window.onload = function() {
-    
-    // --- COUNTDOWN 1: Vakantie in Mei ---
-    const targetMei = new Date(2026, 4, 30); // 30 mei 2026
+    // 1. Vakantie in Mei
+    const targetMei = new Date(2026, 4, 30); 
     const freeDaysMei = [
         new Date(2026, 4, 14).getTime(),
         new Date(2026, 4, 15).getTime()
     ];
     document.getElementById('vrij-mei').innerText = getWorkingDaysLeft(targetMei, freeDaysMei);
 
-    // --- COUNTDOWN 2: Kerstvakantie ---
-    const targetKerst = new Date(2026, 11, 25); // 25 december 2026 (Maand 11 = December)
+    // 2. Kerstvakantie
+    const targetKerst = new Date(2026, 11, 25); 
     document.getElementById('kerst').innerText = getWorkingDaysLeft(targetKerst);
 
-    // --- COUNTDOWN 3: Zomervakantie ---
-    const targetZomer = new Date(2026, 5, 27); // 27 juni 2026 (Maand 5 = Juni)
+    // 3. Zomervakantie
+    const targetZomer = new Date(2026, 5, 27); 
     document.getElementById('zomer').innerText = getWorkingDaysLeft(targetZomer);
 };
